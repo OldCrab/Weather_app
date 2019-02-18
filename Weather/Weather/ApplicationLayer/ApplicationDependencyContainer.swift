69a3bfb0bbac4b67ed76f59ef7344b0cf2d41666
Vasiliev Andrey <scrapland0@gmail.com>
2019-02-18 12:51:33 +0300

    initial commit

---
+++ Weather/Weather/ApplicationLayer/ApplicationDependencyContainer.swift
@@ -0,0 +1,11 @@
+//
+//  ApplicationDependencyContainer.swift
+//  Weather
+//
+//  Created by Andrew Vasiliev on 18/02/2019.
+//  Copyright © 2019 deepkotix. All rights reserved.
+//
+
+final class ApplicationDependencyContainer {
+
+}
